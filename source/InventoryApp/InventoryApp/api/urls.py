ddb032b022138bf228ad7db63b78113b64ec51af
Vinod Gangwar <vinod.gangwar@galepartners.com>
2019-09-06 15:18:55 +0530

    Added import api function

--- source/InventoryApp/InventoryApp/api/urls.py
+++ source/InventoryApp/InventoryApp/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('/data', views.names),
+    path('/import', views.import_data),
 ]
--- source/InventoryApp/InventoryApp/api/views.py
+++ source/InventoryApp/InventoryApp/api/views.py
@@ -1,3 +1,7 @@
 from django.http import JsonResponse
 def names(request):
     return JsonResponse({'names': ['William', 'Rod', 'Grant']})
+
+def import_data(request):
+    # Write your data import logic below
+    return JsonResponse({'names-1': ['William-1', 'Rod-1', 'Grant-1']})
