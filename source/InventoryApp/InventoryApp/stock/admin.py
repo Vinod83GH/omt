782de0033cfddfbbd83c3f825cef0bd3961d166c
Vinod Gangwar <vinod.gangwar@galepartners.com>
2019-09-06 20:17:51 +0530

    Changed stock models

--- source/InventoryApp/InventoryApp/stock/admin.py
+++ source/InventoryApp/InventoryApp/stock/admin.py
@@ -18,7 +18,8 @@ class StockInAdmin(admin.ModelAdmin):
         'total_units',
         # 'get_unit_desc',
         'tax',
-        'total_cost'
+        'total_cost',
+        'entry_date'
     )
 
     list_filter = [
@@ -46,12 +47,15 @@ class StockInAdmin(admin.ModelAdmin):
 
 
 class StockOutAdmin(admin.ModelAdmin):
+    raw_id_fields = ('product_item',)
+    
     list_display = (
-        'get_product_desc',
-        'cost',
-        'get_unit_desc',
-        'tax',
-        'total'
+        'product_item',
+        'unit',
+        'cost_per_unit',
+        'total_units',
+        'total_cost',
+        'out_date'
     )
 
     list_filter = [
@@ -60,20 +64,33 @@ class StockOutAdmin(admin.ModelAdmin):
 
     search_fields = ['product_item__code','product_item__desc', 'product_item__category__desc']
 
-    def get_product_desc(self, obj):
-        return obj.product_item.desc
-
-    def get_unit_desc(self, obj):
-        return obj.unit.desc
-
-    get_product_desc.short_description = 'Product'
-    get_product_desc.boolean = True
-
     class Meta:
         model = StockOut
         verbose_name = 'Stock out'
         verbose_name_plural = 'Stock out'
+        
+
+class StockBalanceAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_item',
+        'unit',
+        'cost_per_unit',
+        'total_units',
+        'total_cost'
+    )
+
+    list_filter = [
+        'product_item__category__desc'
+    ]
+
+    search_fields = ['product_item__code','product_item__desc', 'product_item__category__desc']
+
+    class Meta:
+        model = StockBalance
+        verbose_name = 'StockBalance'
+        verbose_name_plural = 'StockBalance'
 
 
 admin.site.register(StockIn, StockInAdmin)
 admin.site.register(StockOut, StockOutAdmin)
+admin.site.register(StockBalance, StockBalanceAdmin)
--- source/InventoryApp/InventoryApp/stock/models.py
+++ source/InventoryApp/InventoryApp/stock/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.conf import settings
 from InventoryApp.product_catalogue.models import Product, ProductUnit
+from datetime import datetime
 
 class StockIn(models.Model):
     '''Stock entry'''
@@ -10,8 +11,9 @@ class StockIn(models.Model):
     cost_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
     total_units = models.IntegerField(default=0)
     tax = models.DecimalField(max_digits=4, decimal_places=2)
-    manufacturing_date = models.DateTimeField(null=True)
+    manufacturing_date = models.DateTimeField(null=True, blank=True)
     expiry_days = models.IntegerField(default=0)
+    entry_date = models.DateTimeField(null=False, editable=False, default=datetime.now)
     
     @property
     def total_cost(self):
@@ -24,15 +26,28 @@ class StockOut(models.Model):
     '''Stock consumption'''
     product_item = models.ForeignKey(Product, on_delete=models.CASCADE)
     unit = models.ForeignKey(ProductUnit, on_delete=models.CASCADE)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    tax = models.DecimalField(max_digits=4, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    total_units = models.IntegerField(default=0)
+    out_date = models.DateTimeField(null=False, default=datetime.now, editable=False)
+    
+    @property
+    def total_cost(self):
+        if not self.cost_per_unit or not self.total_units:
+            return 0
+        
+        return '{}'.format(self.cost_per_unit * self.total_units)
 
 class StockBalance(models.Model):
-    '''Stock consumption'''
+    '''Stock balance'''
     product_item = models.ForeignKey(Product, on_delete=models.CASCADE)
     unit = models.ForeignKey(ProductUnit, on_delete=models.CASCADE)
-    cost = models.DecimalField(max_digits=10, decimal_places=2)
-    tax = models.DecimalField(max_digits=4, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    total_units = models.IntegerField(default=0)
+    
+    @property
+    def total_cost(self):
+        if not self.cost_per_unit or not self.total_units:
+            return 0
+        
+        return '{}'.format(self.cost_per_unit * self.total_units)
 
